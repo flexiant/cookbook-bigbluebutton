29f49352260812b7d81650eaec784204f3c25efe
Javier Perez-Griffo <javier@besol.es>
2011-10-06 16:58:27 +0200

    Forgot to update metadata.rb of bigbluebutton

--- bigbluebutton/metadata.rb
+++ bigbluebutton/metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "javier@besol.es"
 license          "Apache 2.0"
 description      "Installs/Configures bigbluebutton"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.2.1"
+version          "0.2.2"
 recipe           "bigbluebutton", "Installs and configures bigbluebutton"
 
 %w(apt mysql).each do |recipe_dependency|
