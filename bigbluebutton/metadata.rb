8099b2c754a4f5ee0803ad040b6a3eb008869aa0
Javier Perez-Griffo <javier@javi.local>
2012-04-16 14:05:13 +0200

    Update for the Inclusion of Version 0.8

--- bigbluebutton/metadata.rb
+++ bigbluebutton/metadata.rb
@@ -3,7 +3,7 @@ maintainer_email  "javier@besol.es"
 license           "Apache 2.0"
 description       "Installs bigbluebutton"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.2.42"
+version          "0.2.43"
 recipe           "bigbluebutton", "Installs and configures bigbluebutton"
 
 %w(apt mysql).each do |recipe_dependency|
